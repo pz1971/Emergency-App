e185da15f9b48170bd138b8d1e60fcd030719e96
Parvez Mia <parvez.pz1971@gmail.com>
2020-03-24 01:01:42 +0600

    calling and exit buttons implemented

--- app/src/main/java/com/example/emergencyapp/homeScreen.java
+++ app/src/main/java/com/example/emergencyapp/homeScreen.java
@@ -1,9 +1,15 @@
 package com.example.emergencyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -12,5 +18,85 @@ public class homeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        setupButton1();
+        setupButton2();
+        setupButton3();
+        setupButton4();
+        setupButton5();
+        setupExitButton();
+        setupUserButton();
+        setupInfoButton();
+    }
+
+    private void dialContactPhone(final String phoneNumber) {
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+    }
+
+    protected void setupButton1() {
+        ((ImageButton) findViewById(R.id.button1)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("333");
+            }
+        });
+    }
+    protected void setupButton2(){
+        ((ImageButton)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("999");
+            }
+        });
+    }
+    protected void setupButton3(){
+        ((ImageButton)findViewById(R.id.button3)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("109");
+            }
+        });
+    }
+    protected void setupButton4(){
+        ((ImageButton)findViewById(R.id.button4)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("106");
+            }
+        });
+    }
+    protected void setupButton5(){
+        ((ImageButton)findViewById(R.id.button5)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialContactPhone("1090");
+            }
+        });
+    }
+    protected void setupExitButton(){
+        ((ImageButton)findViewById(R.id.exitButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+            }
+        });
+    }
+    protected void setupUserButton(){
+        ((ImageButton)findViewById(R.id.userButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    protected void setupInfoButton(){
+        ((ImageButton)findViewById(R.id.infoButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }

c326bfde58bbe0d3a119795c3076d7285393cd49
Parvez Mia <parvez.pz1971@gmail.com>
2020-03-23 22:50:52 +0600

    home page design

--- app/src/main/java/com/example/emergencyapp/MainActivity.java
+++ app/src/main/java/com/example/emergencyapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.emergencyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+                Intent i3 = new Intent(MainActivity.this, homeScreen.class);
+                startActivity(i3);
+            }
+        }, 2000);   // auto opens home screen after 2 seconds
     }
 }
---
+++ app/src/main/java/com/example/emergencyapp/homeScreen.java
@@ -0,0 +1,16 @@
+package com.example.emergencyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class homeScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}

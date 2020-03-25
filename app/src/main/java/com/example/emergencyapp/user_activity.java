1ca50fc3020937dfc354f550f6b60c0e89352a23
Parvez Mia <parvez.pz1971@gmail.com>
2020-03-25 16:56:57 +0600

    store and load image for DP

--- app/src/main/java/com/example/emergencyapp/user_activity.java
+++ app/src/main/java/com/example/emergencyapp/user_activity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -18,9 +22,11 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class user_activity extends AppCompatActivity {
@@ -140,6 +146,12 @@ public class user_activity extends AppCompatActivity {
             text = "5"+ birthday.getText().toString() + "\n";
             fos.write(text.getBytes());
 
+            BitmapDrawable drawable = (BitmapDrawable) dp.getDrawable();
+            Bitmap bitmap = drawable.getBitmap();
+
+            text = "6" + saveToInternalStorage(bitmap) + "\n" ;
+            fos.write(text.getBytes());
+
             fos.close();
         } catch (Exception e){
             e.printStackTrace();
@@ -174,11 +186,53 @@ public class user_activity extends AppCompatActivity {
                     case '5':
                         birthday.setText(line.substring(1));
                         break ;
+                    case '6' :
+                        loadImageFromStorage(line.substring(1));
+                        break ;
                 }
             }
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
+
+    private void loadImageFromStorage(String path)
+    {
+
+        try {
+            File f=new File(path, "profile.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            dp.setImageBitmap(b);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
     private void dialContactPhone(final String phoneNumber) {   // dials a number
